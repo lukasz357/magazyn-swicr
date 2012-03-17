c70c8f321862fa8c7b6cee933100dfb412e3b9fa
marcinjasion <marcinjasion@gmail.com>
2012-03-17 22:14:23 +0000

    Dodalem przesuwajacy sie wozek :) ustawienie kroku w MagazynUtils.listStepX i liftStepY

--- Magazyn/src/siwc/magazyn/panels/MapaMagazynu.java
+++ Magazyn/src/siwc/magazyn/panels/MapaMagazynu.java
@@ -3,38 +3,67 @@ package siwc.magazyn.panels;
 import java.awt.Color;
 
 import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
 
 import siwc.magazyn.utils.MagazynUtils;
-import javax.swing.border.LineBorder;
 
 public class MapaMagazynu extends JPanel {
 	private static final long serialVersionUID = -8459627889558824665L;
 	private RegalPanel regalPanel1;
 	private RegalPanel regalPanel2;
+	private Lift lift;
 
 	public MapaMagazynu() {
 		setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
 		setSize(MagazynUtils.mapWidth, MagazynUtils.mapHeight);
 		setBackground(Color.WHITE);
 		setLayout(null);
-		
+
 		regalPanel1 = new RegalPanel();
 		regalPanel1.setBounds(MagazynUtils.regalX, MagazynUtils.getRegalY(0), MagazynUtils.regalWidth, MagazynUtils.regalHeight);
 		add(regalPanel1);
-		
+
 		regalPanel2 = new RegalPanel();
 		regalPanel2.setBounds(MagazynUtils.regalX, MagazynUtils.getRegalY(1), MagazynUtils.regalWidth, MagazynUtils.regalHeight);
 		add(regalPanel2);
-		
+
+		lift = new Lift();
+		lift.setBackground(Color.red);
+		lift.setBounds(lift.getX(), lift.getY(), lift.getXsize(), lift.getYsize());
+		// lift.moveUP(getBounds());
+		add(lift);
+
 	}
 
 	public void zmienKolorBoksu(String r, int x, int y, Color c) {
-		if(r.equals("regal1")) {
+		if (r.equals("regal1")) {
 			regalPanel1.zmienKolorBoksu(x, y, c);
-		} else if(r.equals("regal2")) {
+		} else if (r.equals("regal2")) {
 			regalPanel2.zmienKolorBoksu(x, y, c);
 		}
+	}
+	
+
+	public void moveUp() {
+		lift.moveUp(this.getBounds());
+		repaint();
+	}
+
+	public void moveDown() {
+		lift.moveDown(getBounds());
+		repaint();
+	}
+
+	public void moveLeft() {
+		lift.moveLeft(getBounds());
+		repaint();
+	}
+
+	public void moveRight() {
+		lift.moveRight(getBounds());
+		repaint();
 	}
 
 
 }
+
