103cce8d78bb485aba3b48b6efce39fa4a5d6d87
msarneck <msarneck@gmail.com>
2012-03-18 20:51:05 +0000

    zrobione ,ze wozek nei moze wjezdzac na regaly z zadnej strony.

--- Magazyn/src/siwc/magazyn/panels/MapaMagazynu.java
+++ Magazyn/src/siwc/magazyn/panels/MapaMagazynu.java
@@ -13,7 +13,7 @@ import siwc.magazyn.utils.MagazynUtils;
 
 public class MapaMagazynu extends JPanel {
 	private static final long serialVersionUID = -8459627889558824665L;
-	
+
 	private Logger log = Logger.getLogger(MapaMagazynu.class);
 	private RegalPanel regalPanel1;
 	private RegalPanel regalPanel2;
@@ -37,7 +37,7 @@ public class MapaMagazynu extends JPanel {
 		regalPanel2.setBounds(MagazynUtils.regalX, MagazynUtils.getRegalY(1), MagazynUtils.regalWidth + MagazynUtils.boxSize, MagazynUtils.regalHeight);
 		add(regalPanel2);
 		regaly.add(regalPanel2);
-		
+
 		regalPanel3 = new RegalPanel(false);
 		regalPanel3.setBounds(MagazynUtils.regalX, MagazynUtils.getRegalY(2), MagazynUtils.regalWidth + MagazynUtils.boxSize, MagazynUtils.regalHeight);
 		add(regalPanel3);
@@ -59,42 +59,91 @@ public class MapaMagazynu extends JPanel {
 		}
 	}
 
-	/* sprawdzanie czy nie wjezdzam na regal */
-
 	public void moveUp() {
-		System.out.println("x = " + (lift.getX() + lift.getXsize()) + "y = " + (lift.getY() + lift.getYsize()));
-		System.out.println("regal Panel1 = " + regalPanel1.getBounds());
-		// int xPo = lift.getX() + lift.getDx();
+
 		int xAktualne = lift.getX() + lift.getXsize();
-		int yPo = lift.getY() - lift.getDy();
-		System.out.println("yPo = " + yPo);
-		System.out.println("regal 1 = " + regalPanel1.getBounds().getMinX() + " x max = " + regalPanel1.getBounds().getMaxX());
-		System.out.println("regal 1 :y " + regalPanel1.getBounds().getMinY() + ", " + regalPanel1.getBounds().getMaxY());
-		//sprawdzenie czy w 1 regale
-		if ( ( xAktualne >= regalPanel1.getBounds().getMinX() && xAktualne <= regalPanel1.getBounds().getMaxX() ) && (yPo >= regalPanel1.getBounds().getMinY() && lift.getY() <= regalPanel1.getBounds().getMaxY()) ) {
-			System.err.println("argh !");
-			System.out.println("Panie, co Pan robi ? Na regal wjechac chce Pan ?!");
+		int yPo = lift.getY();
+		boolean isEnteringShelf = false;
+
+		/*
+		 * 1 - czy x nalezy do x regalu 2- czy yWindy wejdzie na dolna krawedz regalu dla kazdego regalu
+		 */
+
+		for (RegalPanel it : regaly) {
+			if (((xAktualne > it.getBounds().getMinX()) && (xAktualne <= it.getBounds().getMaxX())) && (yPo == it.getBounds().getMaxY())) {
+				isEnteringShelf = true;
+			}
+		}
+
+		if (isEnteringShelf) {
+			System.err.println("argh !  Panie, co Pan robi ? Na regal chce Pan wjechac ?!");
 		} else {
 			lift.moveUp(this.getBounds());
 			repaint();
-			
 		}
-		System.out.println("po: x = " + lift.getX() + "y  = " + lift.getY());
 	}
 
 	public void moveDown() {
-		lift.moveDown(getBounds());
-		repaint();
+
+		int xAktualne = lift.getX() + lift.getXsize();
+		double yPo = lift.getY() + lift.getYsize();
+		boolean isEnteringShelf = false;
+
+		for (RegalPanel it : regaly) {
+			if (((xAktualne > it.getBounds().getMinX()) && (xAktualne <= it.getBounds().getMaxX())) && (yPo == it.getBounds().getMinY())) {
+				isEnteringShelf = true;
+			}
+		}
+
+		if (isEnteringShelf) {
+			System.err.println("argh !  Panie, co Pan robi ? Na regal chce Pan wjechac ?!");
+		} else {
+			lift.moveDown(getBounds());
+			repaint();
+		}
 	}
 
 	public void moveLeft() {
-		lift.moveLeft(getBounds());
-		repaint();
+
+		int xPo = lift.getX();
+		double yAktualne = lift.getY();
+		boolean isEnteringShelf = false;
+
+		System.out.println("xPo = " + xPo);
+		System.out.println("regal 3 = " + regalPanel3.getBounds().getMaxX());
+
+		for (RegalPanel it : regaly) {
+			if ((yAktualne >= (it.getBounds().getMinY() - MagazynUtils.liftStepY)) && (yAktualne < it.getBounds().getMaxY()) && (xPo == it.getBounds().getMaxX())) {
+				isEnteringShelf = true;
+			}
+		}
+
+		if (isEnteringShelf) {
+			System.err.println("argh !  Panie, co Pan robi ? Na regal chce Pan wjechac ?!");
+		} else {
+			lift.moveLeft(getBounds());
+			repaint();
+		}
 	}
 
 	public void moveRight() {
-		lift.moveRight(getBounds());
-		repaint();
+
+		int xPo = lift.getX() + lift.getXsize();
+		double yAktualne = lift.getY();
+		boolean isEnteringShelf = false;
+
+		for (RegalPanel it : regaly) {
+			if ((yAktualne >= (it.getBounds().getMinY() - MagazynUtils.liftStepY)) && (yAktualne < it.getBounds().getMaxY()) && (xPo == it.getBounds().getMinX())) {
+				isEnteringShelf = true;
+			}
+		}
+
+		if (isEnteringShelf) {
+			System.err.println("argh !  Panie, co Pan robi ? Na regal chce Pan wjechac ?!");
+		} else {
+			lift.moveRight(getBounds());
+			repaint();
+		}
 	}
 
 }
