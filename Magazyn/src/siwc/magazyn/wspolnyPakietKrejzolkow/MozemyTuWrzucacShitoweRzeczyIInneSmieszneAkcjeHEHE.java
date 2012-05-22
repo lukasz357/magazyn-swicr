b88ec8bca94905a189617d301246767c43a29036
lukasz357 <lukasz357@gmail.com>
2012-05-22 22:14:50 +0000

    Kolejne drobne usprawnienia

--- Magazyn/src/siwc/magazyn/wspolnyPakietKrejzolkow/MozemyTuWrzucacShitoweRzeczyIInneSmieszneAkcjeHEHE.java
+++
@@ -1,9 +0,0 @@
-package siwc.magazyn.wspolnyPakietKrejzolkow;
-
-public class MozemyTuWrzucacShitoweRzeczyIInneSmieszneAkcjeHEHE {
-
-	//śmierdzicie!
-	// chyba ty
-	// chyba ja (by jasion ;d;d;d )
-	// mareg umyj stopy
-}
