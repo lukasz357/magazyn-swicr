d57816350e9b5e59a80db93614a5aa28db4239d6
lukasz357@gmail.com <lukasz357@gmail.com@70a15d6a-5ceb-4e16-4c0e-e698bda53fd6>
2012-05-24 01:34:36 +0000

    Ha. Nawet warninga nie ma!

--- trunk/Magazyn/src/siwc/magazyn/logic/IOLogic.java
+++ trunk/Magazyn/src/siwc/magazyn/logic/IOLogic.java
@@ -6,7 +6,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Set;
 import java.util.TreeMap;
 
 import org.apache.log4j.Logger;
